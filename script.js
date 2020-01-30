0a75d46334340d1097ef03f6a44985ec46757ffa
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-30 15:39:06 -0500

    Updated JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+var nineAmElement = document.querySelector("#nine-am");
+var tenAmElement = document.querySelector("#ten-am");
+var elevenAmElement = document.querySelector("#eleven-am");
+var twelevePmElement = document.querySelector("#tweleve-pm");
+var onePmElement = document.querySelector("#one-pm");
+var twoPmElement = document.querySelector("#two-pm");
+var threePmElement = document.querySelector("#three-pm");
+var fourPmElement = document.querySelector("#four-pm");
+var fivePmElement = document.querySelector("#five-pm");
+var saveBtn = document.querySelector("#save");
 var today = document.querySelector("#currentDay");
+var momentElement = moment().format('llll');
 
- function TodayDate(){
-moment().format("MM Do YY");
-console.log(todayDate);
 
- }
+
+ today.innerHTML=(momentElement);
+
+$("#save").on("click",function(){
+    console.log("hi");
+    
+    event.preventDefault();
+
+    var nineAM = document.querySelector("#nine-am").value;
+    var tenAM = document.querySelector("#ten-am").value;
+    var elevenAM = document.querySelector("#eleven-am").value;
+    var twelvePM = document.querySelector("#twelve-pm").value;
+    var onePM = document.querySelector("#one-pm").value;
+    var twoPM = document.querySelector("#two-pm").value;
+    var threePM = document.querySelector("#three-pm").value;
+    var fourPM = document.querySelector("#four-pm").value;
+    var fivePM = document.querySelector("#five-pm").value;
+
+    if(nineAM === ""){
+        displayMessage("error", "Email cannot be blank");
+
+        localStorage.setItem("#nine-am", nineAm);
+
+
+    }
+
+    
+    
+   
+});
