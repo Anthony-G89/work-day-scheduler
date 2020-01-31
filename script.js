d6eac4710f267daa7f064130f2a364d760d9dd98
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-31 02:29:00 -0500

    trying to get user input to stay after reloading the page

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-var nineAmElement = document.querySelector("#nine-am");
-var tenAmElement = document.querySelector("#ten-am");
-var elevenAmElement = document.querySelector("#eleven-am");
-var twelevePmElement = document.querySelector("#tweleve-pm");
-var onePmElement = document.querySelector("#one-pm");
-var twoPmElement = document.querySelector("#two-pm");
-var threePmElement = document.querySelector("#three-pm");
-var fourPmElement = document.querySelector("#four-pm");
-var fivePmElement = document.querySelector("#five-pm");
+var nineAmElement = document.querySelector("#user-input9");
+var tenAmElement = document.querySelector("#user-input10");
+var elevenAmElement = document.querySelector("#user-input11");
+var twelevePmElement = document.querySelector("#user-input12");
+var onePmElement = document.querySelector("#user-input1");
+var twoPmElement = document.querySelector("#user-input2");
+var threePmElement = document.querySelector("#user-input3");
+var fourPmElement = document.querySelector("#user-input4");
+var fivePmElement = document.querySelector("#user-input5");
 var saveBtn = document.querySelector("#save");
 var today = document.querySelector("#currentDay");
 var momentElement = moment().format('llll');
 
 
 
+
  today.innerHTML=(momentElement);
 
 $("#save").on("click",function(){
-    console.log("hi");
-    
     event.preventDefault();
 
-    var nineAM = document.querySelector("#nine-am").value;
-    var tenAM = document.querySelector("#ten-am").value;
-    var elevenAM = document.querySelector("#eleven-am").value;
-    var twelvePM = document.querySelector("#twelve-pm").value;
-    var onePM = document.querySelector("#one-pm").value;
-    var twoPM = document.querySelector("#two-pm").value;
-    var threePM = document.querySelector("#three-pm").value;
-    var fourPM = document.querySelector("#four-pm").value;
-    var fivePM = document.querySelector("#five-pm").value;
+    var nineElem = nineAmElement.value;
+    var tenAM = document.querySelector("#user-input10").value;
+    var elevenAM = document.querySelector("#user-input11").value;
+    var twelvePM = document.querySelector("#user-input12").value;
+    var onePM = document.querySelector("#user-input1").value;
+    var twoPM = document.querySelector("#user-input2").value;
+    var threePM = document.querySelector("#user-input3").value;
+    var fourPM = document.querySelector("#user-input4").value;
+    var fivePM = document.querySelector("#user-input5").value;
 
-    if(nineAM === ""){
-        displayMessage("error", "Email cannot be blank");
-
-        localStorage.setItem("#nine-am", nineAm);
-
-
-    }
-
-    
-    
    
+        localStorage.setItem("9AM-UR", JSON.stringify (nineElem));
+        JSON.parse(localStorage.getItem(nineElem));
+        nineAmElement.textcontent = nineElem;
+
+       
 });
+
+function storeInput(){
+    
+}
+function renderInput(){
+
+}
